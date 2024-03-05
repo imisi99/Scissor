a1b65cbbfff3fdbe83f05020d7a8f1dca0438bb8
imisi99 <isongrichard234@gmail.com>
2024-03-05 15:28:49 +0100

    Initial commit

---
+++ app/routers/user.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+from pydantic import BaseModel
+
+
+user = APIRouter()
+
+#user signup 
+#User login 
+#get user details 
+#user update details
+#user forgot password 
