3343d655abad783a9500d43d2d27b05d32985a2f
imisi99 <isongrichard234@gmail.com>
2024-03-11 13:18:56 +0100

    Changing databse url to live database

--- app/schemas/database.py
+++ app/schemas/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-database_url = 'postgresql://postgres:Imisioluwa234.@localhost/Scissors'
+database_url = 'postgresql://ivgzbexn:QDKx7C6ImQ7YAvS7f6XP1KivA-NgUNui@trumpet.db.elephantsql.com/ivgzbexn'
 engine = create_engine(database_url)
 begin = sessionmaker(bind= engine, autoflush= False, autocommit = False)
 data = declarative_base()
